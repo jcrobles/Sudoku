f07ee3b02843441ac441f043d6f4ee976c14c407
Drew Malin <drew.malin@gmail.com>
2014-05-18 13:31:37 -0700

    temp

--- main.py
+++ main.py
@@ -9,11 +9,3 @@ BOARD = [
     [' ', ' ', ' ', 4, 1, 9, ' ', ' ', 5],
     [' ', ' ', ' ', ' ', 8, ' ', ' ', 7, 9]]
 
-def print_board(board):
-    width = # enter the width!
-    height = # enter the height!
-    for row in range(width):
-        for col in range(height):
-            # print the board!
-
-print_board(BOARD)
