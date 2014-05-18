bef6b9d35f8e79021970d75ca1cf08c74406020d
Drew Malin <drew.malin@gmail.com>
2014-05-18 12:42:50 -0700

    print

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ BOARD = [
     [],
     [],
     []]
+
+def print_board(board):
+    width = 9
+    height = 9
+    border = "+-----------+-----------+-----------+"
+    print border
+    for row in range(width):
+        print "|",
+        for col in range(height):
+            print BOARD[row][col],
+            if (col + 1) % 3 == 0:
+                print "|",
+            else:
+                print " ",
+        if (row + 1) % 3 == 0:
+            print border
+        else:
+            print "\n"
+
+print_board(BOARD)
